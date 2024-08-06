fe71c43e7700ae33f521c03af1cb2cdbbb484aac
MicolGiannelli19 <micol.giannelli@outlook.com>
2024-08-06 23:11:00 +0100

    documenting views.py and runnable script

---
+++ commerce/auctions/script.py
@@ -0,0 +1,16 @@
+import django
+import os
+
+# Set up Django settings
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'commerce.settings')
+django.setup()
+
+# Now you can import Django models
+from auctions.models import Listing
+
+print("Printing Lisings: \n")
+print(Listing.objects.all())
+
+
+current = Listing.objects.values_list('category', flat=True).distinct()
+print(current)
